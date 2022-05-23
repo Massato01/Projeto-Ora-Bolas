5993b24a8c3aa2d999a86189e128666c1e3b4f95
Massato01 <massatohc@gmail.com>
2022-05-23 18:00:25 -0300

    PAJE OLHA O VISUAL_3D

--- visual_3d.py
+++ visual_3d.py
@@ -1,10 +1,17 @@
 # ====================================================
 # ------------------  BIBLIOTECAS --------------------
-from robo    import *
-# from graficos import *
+from robo    import * 
+from graficos import *
 from vpython import *
 from sympy   import *
 # ====================================================
+
+# Listas para o momento de interceptação
+intercepto_xRobo = []
+intercepto_yRobo = []
+intercepto_tRobo = []
+distancia_robo_bola = [] # NÃO É A FUNÇÃO "dist_robo_bola()"
+
 
 # ====================================================
 #        ORIENTAÇÕES PARA INICIAR A ANIMAÇÃO
@@ -13,15 +20,15 @@ scene = canvas(width = 800, height = 600)
 
 scene.caption = f'''
 <h1>ORA BOLAS!</h1>
-<h2><strong>Precione qualquer tecla para iniciar a animação!</strong></h2>
-<h3>Coordenadas escolhidas para a posição inicial do robô:
+<h3><strong>Precione qualquer tecla para iniciar a animação!</strong>\n
+Coordenadas escolhidas para a posição inicial do robô:
 [  X: {x_robo}  ]
 [  Y: {y_robo}  ]</h3>
 '''
 
 
 # BOLA DE REFERENCIA DOS PONTOS (0, 0)
-bola_referencia = sphere(pos = vector(0, 0, 0), radius = 0.1)
+# bola_referencia = sphere(pos = vector(0, 0, 0), radius = 0.1)
 
 # ====----====----====----====----====----====----====
 #             CRIANDO O CAMPO DE FUTEBOL
@@ -64,7 +71,9 @@ BOLA = sphere(pos = vector(bola_x_pos[0], bola_y_pos[0], 0.07), color = color.re
 # ====----====----====----====----====----====----====
 ROBO_CORPO = cylinder(pos = vector(x_robo, y_robo, 0.3),
                       color = color.black,
-                      width = 0.25, length = 0.25, height = 0.25)
+                      width = 0.25, length = 0.25, height = 0.25,
+                      make_trail = True, trail_color = color.cyan,
+                      interval = 1)
 
 RODA_DIREITA = cylinder(pos=vector(x_robo, y_robo - 0.13, 0.142),
                          color = color.white,
@@ -93,6 +102,9 @@ while True:
         # Contador para as coordenadas da bola
         cont_bola = 0
 
+        # Contador para as coordenadas do robô
+        cont_robo = 0
+
         # [ Enquanto o contador for menor q "bola_x_pos" e não sair das dimensões 
         #   do campo ]
         while cont_bola < len(bola_x_pos) and BOLA.pos.x - BOLA.radius > -CAMPO.length / 2:
@@ -100,11 +112,65 @@ while True:
             # FPS (Frames Por Segundo) da animação
             rate(120)
             
-            # [ AS DIMENSÕES USADAS NO CAMPO FORAM APENAS X E Z ]
+            # --------------------
+            # MOVIMENTAÇÃO DA BOLA
+            # --------------------
             BOLA.pos.x += (bola_x_pos[cont_bola]) - BOLA.pos.x
             BOLA.pos.y += (bola_y_pos[cont_bola]) - BOLA.pos.y
             
             cont_bola += 1
-        
+
+            # --------------------
+            # MOVIMENTAÇÃO DO ROBÔ
+            # --------------------
+            if cont_robo < len(lista_xRobo):
+                # Movimentação do corpo do robô
+                ROBO_CORPO.pos.x = lista_xRobo[cont_robo]
+                ROBO_CORPO.pos.y = lista_yRobo[cont_robo]
+
+                # Movimentação das rodas do robô
+                RODA_DIREITA.pos.x = lista_xRobo[cont_robo]
+                RODA_DIREITA.pos.y = lista_yRobo[cont_robo] + ROBO_CORPO.height-0.12
+
+                RODA_ESQUERDA.pos.x = lista_xRobo[cont_robo]
+                RODA_ESQUERDA.pos.y = lista_yRobo[cont_robo] - ROBO_CORPO.height+0.12
+
+                cont_robo += 1
+            # -----------------------------------------
+            # MOMENTO DA INTERCEPTAÇÃO DO ROBÔ E A BOLA
+            # -----------------------------------------
+            if (BOLA.pos.x > ROBO_CORPO.pos.x - (ROBO_CORPO.height-0.12) and
+                BOLA.pos.y >= (ROBO_CORPO.height-0.12)):
+                
+                if intercepto is not False:
+                    intercepto_xRobo.append(ROBO_CORPO.pos.x)
+                    intercepto_yRobo.append(ROBO_CORPO.pos.y)
+                    intercepto_tRobo.append(bola_t_pos[cont_bola])
+
+                    distancia_robo_bola.append(dist_robo_bola(
+                        ROBO_CORPO.pos.x - ROBO_CORPO.width / 10,
+                        ROBO_CORPO.pos.y,
+                        BOLA.pos.x,
+                        BOLA.pos.y
+                    ))
+
+                    scene.append_to_caption(
+                        f'''<h2>INTERCEPTOU!\nPonto X da interceptação: {menor_distX:.1f}\nPonto Y da interceptação: {menor_distY:.1f}\nPonto T da interceptação: {menor_distT:.1f}</h2>'''
+                    )
+
+                break
+            
+            if BOLA.pos.x <= 6 and BOLA.pos.y <= 9:
+                distancia_robo_bola.append(dist_robo_bola(
+                    ROBO_CORPO.pos.x,
+                    ROBO_CORPO.pos.y,
+                    BOLA.pos.x,
+                    BOLA.pos.y
+                ))
+
+                intercepto_xRobo.append(ROBO_CORPO.pos.x)
+                intercepto_yRobo.append(ROBO_CORPO.pos.y)
+                intercepto_tRobo.append(bola_t_pos[cont_bola])
+
         # ENCERRA O LOOP INFINITO
         break
